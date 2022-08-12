6359663659a2e21003b4f0e627151060cae38741
Stan Papadopoulos <stan.papadopoulos@gmail.com>
2022-08-12 09:23:12 +0200

    Add test for nuclear repulsion and create constructor for Atom struct.

--- src/tools/geometry.rs
+++ src/tools/geometry.rs
@@ -1,23 +1,42 @@
+#[derive(Copy, Clone)]
 pub struct Atom {
-  pub charge: f64,
-  pub mass: f64,
-  pub origin: [f64; 3],
+    charge: f32,
+    mass: f32,
+    origin: [f64; 3],
 }
 
 impl Atom {
-
-  pub fn scale_coords(&mut self, factor: &f64) {
-    for i in 0..3 {
-      self.origin[i] *= factor;
+    pub fn new(charge: f32, mass: f32, origin: [f64; 3]) -> Self {
+        Self {
+            charge,
+            mass,
+            origin,
+        }
     }
-  }
 
+    pub fn scale_coords(&mut self, factor: &f64) {
+        for i in 0..3 {
+            self.origin[i] *= factor;
+        }
+    }
+
+    pub fn charge(&self) -> f32 {
+        self.charge
+    }
+
+    pub fn mass(&self) -> f32 {
+        self.mass
+    }
+
+    pub fn origin(&self) -> [f64; 3] {
+        self.origin
+    }
 }
 
 pub fn distance(a: &Atom, b: &Atom) -> f64 {
-  let dx = a.origin[0] - b.origin[0];
-  let dy = a.origin[1] - b.origin[1];
-  let dz = a.origin[2] - b.origin[2];
+    let dx = a.origin[0] - b.origin[0];
+    let dy = a.origin[1] - b.origin[1];
+    let dz = a.origin[2] - b.origin[2];
 
-  (dx*dx + dy*dy + dz*dz).sqrt()
+    (dx * dx + dy * dy + dz * dz).sqrt()
 }
--- src/tools/gto_integrals/nuclear_repulsion.rs
+++ src/tools/gto_integrals/nuclear_repulsion.rs
@@ -1,13 +1,33 @@
-use crate::tools::geometry::{Atom, distance};
+use crate::tools::geometry::{distance, Atom};
 
 pub fn nuclear_repulsion(atoms: &Vec<Atom>) -> f64 {
-  
-  let n = atoms.len();
-  let mut vnn = 0.0;
-  for i in 0..n {
-    for j in 0..i {
-      vnn += atoms[i].charge * atoms[j].charge / distance(&atoms[i], &atoms[j]);
+    let n = atoms.len();
+    let mut vnn = 0.0;
+    for i in 0..n {
+        for j in 0..i {
+            vnn += atoms[i].charge() as f64 * atoms[j].charge() as f64
+                / distance(&atoms[i], &atoms[j]);
+        }
     }
-  }
-  vnn
+    vnn
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tools::constants;
+
+    #[test]
+    fn nuclear_repulsion() {
+        let o = Atom::new(8.0, 15.999, [0.0000000000, 0.0000000000, -0.1190150726]);
+        let h1 = Atom::new(1.0, 1.008, [0.7685504811, 0.0000000000, 0.4760602904]);
+        let h2 = Atom::new(1.0, 1.008, [-0.7685504811, 0.0000000000, 0.4760602904]);
+
+        let mut atoms = vec![o, h1, h2];
+        for i in 0..atoms.len() {
+            atoms[i].scale_coords(&constants::_ANG_AU);
+        }
+
+        assert_eq!(super::nuclear_repulsion(&atoms), 9.055003146181436);
+    }
 }
