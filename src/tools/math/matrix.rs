8e5c7eac85a513f481613fb8103b25f389a140ec
Stan Papadopoulos <stan.papadopoulos@gmail.com>
2022-08-13 13:11:20 +0200

    Add some basic operators to BaseMatrix

--- src/tools/math/matrix.rs
+++ src/tools/math/matrix.rs
@@ -1,9 +1,23 @@
 use crate::tools::math::scalar::Scalar;
 use std::iter::FromIterator;
-use std::ops::{Index, IndexMut, Mul, MulAssign};
+use std::ops::{Add, AddAssign, Index, IndexMut, Mul, MulAssign, Sub, SubAssign};
 
 pub type FMatrix = BaseMatrix<f64>;
 pub type IMatrix = BaseMatrix<i64>;
+
+// // todo: is this OK? or should I use some sort of Matrix trait?
+// pub struct MatrixSquare<T: Scalar>(BaseMatrix<T>);
+// pub type FMatrixSquare = MatrixSquare<f64>;
+// pub type IMatrixSquare = MatrixSquare<i64>;
+//
+// pub struct MatrixSym<T: Scalar>(MatrixSquare<T>);
+// pub type FMatrixSym = MatrixSquare<f64>;
+// pub type IMatrixSym = MatrixSquare<i64>;
+
+/*
+ * note: cannot use macros from forward_ref, because BaseMatrix<T> does
+ *       implement Copy...
+ */
 
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct BaseMatrix<T: Scalar> {
@@ -30,8 +44,16 @@ impl<T: Scalar> BaseMatrix<T> {
         self.elem.len()
     }
 
-    pub fn shape(&self) -> [usize; 2] {
-        [self.rows, self.cols]
+    pub fn shape(&self) -> (usize, usize) {
+        (self.rows, self.cols)
+    }
+
+    fn is_ok(&self) -> bool {
+        self.size() == self.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.len() == 0
     }
 }
 
@@ -46,7 +68,7 @@ impl<T: Scalar> BaseMatrix<T> {
         BaseMatrix::<T> {
             rows,
             cols,
-            elem: vec![T::default(); rows * cols],
+            elem: Vec::with_capacity(rows * cols),
         }
     }
 
@@ -64,6 +86,10 @@ impl<T: Scalar> BaseMatrix<T> {
         }
     }
 
+    pub fn zeros(rows: usize, cols: usize) -> BaseMatrix<T> {
+        Self::new_with_value(rows, cols, T::default())
+    }
+
     pub fn init(&mut self, value: T) {
         self.elem = vec![value; self.size()];
     }
@@ -75,17 +101,31 @@ impl<T: Scalar> BaseMatrix<T> {
     }
 
     pub fn print(&self) {
+        if self.is_empty() {
+            println!("Empty matrix. Nothing to print.");
+            return;
+        }
+
         for i in 0..self.rows {
             for j in 0..self.cols {
-                print!("{:12.8} ", self[[i, j]]);
+                print!("{:12.8} ", self[(i, j)]);
             }
             println!()
         }
     }
 
     pub fn transpose(&mut self) {
-        if self.len() == 0 {
+        // vector has not been initialized, so we just swap the rows and cols
+        if self.is_empty() {
+            let rows = self.rows;
+            self.rows = self.cols;
+            self.cols = rows;
+
             return;
+        }
+
+        if !self.is_ok() {
+            panic!("[transpose] Size does not match len!");
         }
 
         if self.rows == self.cols {
@@ -93,9 +133,9 @@ impl<T: Scalar> BaseMatrix<T> {
             let mut x: T;
             for i in 0..self.rows {
                 for j in 0..i {
-                    x = self[[i, j]];
-                    self[[i, j]] = self[[j, i]];
-                    self[[j, i]] = x;
+                    x = self[(i, j)];
+                    self[(i, j)] = self[(j, i)];
+                    self[(j, i)] = x;
                 }
             }
         } else {
@@ -103,43 +143,222 @@ impl<T: Scalar> BaseMatrix<T> {
             self.del();
             *self = BaseMatrix::<T>::new(mat.cols, mat.rows);
 
-            for i in 0..mat.rows {
-                for j in 0..mat.cols {
-                    self[[j, i]] = mat[[i, j]]
+            for i in 0..self.rows {
+                for j in 0..self.cols {
+                    self.elem.push(mat[(j, i)]);
                 }
             }
         }
     }
 }
 
-impl<T: Scalar> Index<[usize; 2]> for BaseMatrix<T> {
+// ---------------------------------------------------------------------
+// Index
+// ---------------------------------------------------------------------
+
+impl<T: Scalar> Index<(usize, usize)> for BaseMatrix<T> {
     type Output = T;
 
-    fn index(&self, index: [usize; 2]) -> &Self::Output {
-        &self.elem[index[0] * self.cols + index[1]]
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        &self.elem[index.0 * self.cols + index.1]
     }
 }
 
-impl<T: Scalar> IndexMut<[usize; 2]> for BaseMatrix<T> {
-    fn index_mut(&mut self, index: [usize; 2]) -> &mut Self::Output {
-        &mut self.elem[index[0] * self.cols + index[1]]
+impl<T: Scalar> IndexMut<(usize, usize)> for BaseMatrix<T> {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
+        &mut self.elem[index.0 * self.cols + index.1]
     }
 }
 
-// allows BaseMatrix * scalar
+// ---------------------------------------------------------------------
+// Add
+// ---------------------------------------------------------------------
+
+// BaseMatrix<T> = BaseMatrix<T> + BaseMatrix<T>
+impl<T: Scalar + Add<Output = T>> Add<BaseMatrix<T>> for BaseMatrix<T> {
+    type Output = BaseMatrix<T>;
+
+    fn add(self, rhs: BaseMatrix<T>) -> Self::Output {
+        if self.shape() != rhs.shape() {
+            panic!("[Add] Matrix dimensions are not the same.");
+        }
+
+        let mut mat = Self::Output::new(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                mat.elem.push(self[(i, j)] + rhs[(i, j)]);
+            }
+        }
+
+        mat
+    }
+}
+
+// BaseMatrix<T> = BaseMatrix<T> + &BaseMatrix<T>
+impl<T: Scalar + Add<Output = T>> Add<&BaseMatrix<T>> for BaseMatrix<T> {
+    type Output = BaseMatrix<T>;
+
+    fn add(self, rhs: &BaseMatrix<T>) -> Self::Output {
+        if self.shape() != rhs.shape() {
+            panic!("[Add] Matrix dimensions are not the same.");
+        }
+
+        let mut mat = Self::Output::new(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                mat.elem.push(self[(i, j)] + rhs[(i, j)]);
+            }
+        }
+
+        mat
+    }
+}
+
+// BaseMatrix<T> = &BaseMatrix<T> + BaseMatrix<T>
+impl<T: Scalar + Add<Output = T>> Add<BaseMatrix<T>> for &BaseMatrix<T> {
+    type Output = BaseMatrix<T>;
+
+    fn add(self, rhs: BaseMatrix<T>) -> Self::Output {
+        if self.shape() != rhs.shape() {
+            panic!("[Add] Matrix dimensions are not the same.");
+        }
+
+        let mut mat = Self::Output::new(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                mat.elem.push(self[(i, j)] + rhs[(i, j)]);
+            }
+        }
+
+        mat
+    }
+}
+
+// BaseMatrix<T> = &BaseMatrix<T> + &BaseMatrix<T>
+impl<T: Scalar + Add<Output = T>> Add<&BaseMatrix<T>> for &BaseMatrix<T> {
+    type Output = BaseMatrix<T>;
+
+    fn add(self, rhs: &BaseMatrix<T>) -> Self::Output {
+        if self.shape() != rhs.shape() {
+            panic!("[Add] Matrix dimensions are not the same.");
+        }
+
+        let mut mat = Self::Output::new(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                mat.elem.push(self[(i, j)] + rhs[(i, j)]);
+            }
+        }
+
+        mat
+    }
+}
+
+// ---------------------------------------------------------------------
+// AddAssign
+// ---------------------------------------------------------------------
+
+// allows BaseMatrix += BaseMatrix
+impl<T: Scalar> AddAssign<BaseMatrix<T>> for BaseMatrix<T> {
+    fn add_assign(&mut self, rhs: Self) {
+        if self.shape() != rhs.shape() {
+            panic!("[AddAssign] Matrix dimensions are not the same.");
+        }
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                self[(i, j)] += rhs[(i, j)];
+            }
+        }
+    }
+}
+
+// allows BaseMatrix += &BaseMatrix
+impl<T: Scalar> AddAssign<&BaseMatrix<T>> for BaseMatrix<T> {
+    fn add_assign(&mut self, rhs: &Self) {
+        if self.shape() != rhs.shape() {
+            panic!("[AddAssign] Matrix dimensions are not the same.");
+        }
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                self[(i, j)] += rhs[(i, j)];
+            }
+        }
+    }
+}
+
+// ---------------------------------------------------------------------
+// Mul
+// ---------------------------------------------------------------------
+
+// BaseMatrix = BaseMatrix * scalar
 impl<T: Scalar> Mul<T> for BaseMatrix<T>
 where
     Vec<T>: FromIterator<<T as Mul>::Output>,
 {
     type Output = BaseMatrix<T>;
 
-    fn mul(self, scalar: T) -> Self::Output {
-        Self {
-            elem: self.elem.into_iter().map(|v| v * scalar).collect(),
+    fn mul(self, rhs: T) -> Self::Output {
+        Self::Output {
+            elem: self.elem.into_iter().map(|v| v * rhs).collect(),
             ..self
         }
     }
 }
+
+// BaseMatrix = BaseMatrix * &scalar
+impl<T: Scalar> Mul<&T> for BaseMatrix<T>
+where
+    Vec<T>: FromIterator<<T as Mul>::Output>,
+{
+    type Output = BaseMatrix<T>;
+
+    fn mul(self, rhs: &T) -> Self::Output {
+        Self::Output {
+            elem: self.elem.into_iter().map(|v| v * *rhs).collect(),
+            ..self
+        }
+    }
+}
+
+// BaseMatrix = &BaseMatrix * scalar
+impl<T: Scalar> Mul<T> for &BaseMatrix<T>
+where
+    Vec<T>: FromIterator<<T as Mul>::Output>,
+{
+    type Output = BaseMatrix<T>;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Self::Output {
+            elem: self.elem.iter().map(|v| *v * rhs).collect(),
+            ..*self
+        }
+    }
+}
+
+// BaseMatrix = &BaseMatrix * &scalar
+impl<T: Scalar> Mul<&T> for &BaseMatrix<T>
+where
+    Vec<T>: FromIterator<<T as Mul>::Output>,
+{
+    type Output = BaseMatrix<T>;
+
+    fn mul(self, rhs: &T) -> Self::Output {
+        Self::Output {
+            elem: self.elem.iter().map(|v| *v * *rhs).collect(),
+            ..*self
+        }
+    }
+}
+
+// ---------------------------------------------------------------------
+// MulAssign
+// ---------------------------------------------------------------------
 
 // allows BaseMatrix *= scalar
 impl<T: Scalar> MulAssign<T> for BaseMatrix<T> {
@@ -150,49 +369,196 @@ impl<T: Scalar> MulAssign<T> for BaseMatrix<T> {
     }
 }
 
+// allows BaseMatrix *= &scalar
+impl<T: Scalar> MulAssign<&T> for BaseMatrix<T> {
+    fn mul_assign(&mut self, rhs: &T) {
+        for x in &mut self.elem {
+            *x *= *rhs;
+        }
+    }
+}
+
+// ---------------------------------------------------------------------
+// Sub
+// ---------------------------------------------------------------------
+
+// BaseMatrix<T> = BaseMatrix<T> - BaseMatrix<T>
+impl<T: Scalar + Sub<Output = T>> Sub<BaseMatrix<T>> for BaseMatrix<T> {
+    type Output = BaseMatrix<T>;
+
+    fn sub(self, rhs: BaseMatrix<T>) -> Self::Output {
+        if self.shape() != rhs.shape() {
+            panic!("[Sub] Matrix dimensions are not the same.");
+        }
+
+        let mut mat = Self::Output::new(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                mat.elem.push(self[(i, j)] - rhs[(i, j)]);
+            }
+        }
+
+        mat
+    }
+}
+
+// BaseMatrix<T> = BaseMatrix<T> - &BaseMatrix<T>
+impl<T: Scalar + Sub<Output = T>> Sub<&BaseMatrix<T>> for BaseMatrix<T> {
+    type Output = BaseMatrix<T>;
+
+    fn sub(self, rhs: &BaseMatrix<T>) -> Self::Output {
+        if self.shape() != rhs.shape() {
+            panic!("[Sub] Matrix dimensions are not the same.");
+        }
+
+        let mut mat = Self::Output::new(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                mat.elem.push(self[(i, j)] - rhs[(i, j)]);
+            }
+        }
+
+        mat
+    }
+}
+
+// BaseMatrix<T> = &BaseMatrix<T> - BaseMatrix<T>
+impl<T: Scalar + Sub<Output = T>> Sub<BaseMatrix<T>> for &BaseMatrix<T> {
+    type Output = BaseMatrix<T>;
+
+    fn sub(self, rhs: BaseMatrix<T>) -> Self::Output {
+        if self.shape() != rhs.shape() {
+            panic!("[Sub] Matrix dimensions are not the same.");
+        }
+
+        let mut mat = Self::Output::new(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                mat.elem.push(self[(i, j)] - rhs[(i, j)]);
+            }
+        }
+
+        mat
+    }
+}
+
+// BaseMatrix<T> = &BaseMatrix<T> - &BaseMatrix<T>
+impl<T: Scalar + Sub<Output = T>> Sub<&BaseMatrix<T>> for &BaseMatrix<T> {
+    type Output = BaseMatrix<T>;
+
+    fn sub(self, rhs: &BaseMatrix<T>) -> Self::Output {
+        if self.shape() != rhs.shape() {
+            panic!("[Sub] Matrix dimensions are not the same.");
+        }
+
+        let mut mat = Self::Output::new(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                mat.elem.push(self[(i, j)] - rhs[(i, j)]);
+            }
+        }
+
+        mat
+    }
+}
+
+// ---------------------------------------------------------------------
+// SubAssign
+// ---------------------------------------------------------------------
+
+// allows BaseMatrix -= BaseMatrix
+impl<T: Scalar> SubAssign<BaseMatrix<T>> for BaseMatrix<T> {
+    fn sub_assign(&mut self, rhs: BaseMatrix<T>) {
+        if self.shape() != rhs.shape() {
+            panic!("[SubAssign] Matrix dimensions are not the same.");
+        }
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                self[(i, j)] -= rhs[(i, j)];
+            }
+        }
+    }
+}
+
+// allows BaseMatrix -= &BaseMatrix
+impl<T: Scalar> SubAssign<&BaseMatrix<T>> for BaseMatrix<T> {
+    fn sub_assign(&mut self, rhs: &Self) {
+        if self.shape() != rhs.shape() {
+            panic!("[SubAssign] Matrix dimensions are not the same.");
+        }
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                self[(i, j)] -= rhs[(i, j)];
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const N: usize = 5;
+    const N2: usize = N * N;
+
+    // todo: add cases that panic
+
     #[test]
     fn new() {
         let mat = FMatrix {
-            rows: 5,
-            cols: 5,
-            elem: vec![0.0; 25],
+            rows: N,
+            cols: N,
+            elem: Vec::with_capacity(N2),
         };
 
-        assert_eq!(mat, FMatrix::new(5, 5));
+        assert_eq!(mat, FMatrix::new(N, N));
     }
 
     #[test]
     fn new_with_value() {
         let mat = FMatrix {
-            rows: 5,
-            cols: 5,
-            elem: vec![5.0; 25],
+            rows: N,
+            cols: N,
+            elem: vec![5.0; N2],
         };
 
-        assert_eq!(mat, FMatrix::new_with_value(5, 5, 5.0));
+        assert_eq!(mat, FMatrix::new_with_value(N, N, 5.0));
+    }
+
+    #[test]
+    fn zeros() {
+        let mat = FMatrix {
+            rows: N,
+            cols: N,
+            elem: vec![0.0; N2],
+        };
+
+        assert_eq!(mat, FMatrix::zeros(N, N));
     }
 
     #[test]
     fn init() {
-        let mut mat = FMatrix::new(5, 5);
+        let mut mat = FMatrix::new(N, N);
         mat.init(5.0);
 
-        assert_eq!(mat, FMatrix::new_with_value(5, 5, 5.0));
+        assert_eq!(mat, FMatrix::new_with_value(N, N, 5.0));
     }
 
     #[test]
     fn index() {
         let mat = IMatrix {
-            rows: 5,
-            cols: 5,
-            elem: (0..25).collect(),
+            rows: N,
+            cols: N,
+            elem: (0..N2 as i64).collect(),
         };
 
-        assert_eq!(mat[[3, 0]], 15);
+        assert_eq!(mat[(3, 0)], 15);
     }
 
     #[test]
@@ -231,18 +597,89 @@ mod tests {
     }
 
     #[test]
-    fn matrix_scalar_mul() {
-        let mat = FMatrix::new_with_value(5, 5, 1.0);
-        let new_mat = mat * 2.0;
+    fn add() {
+        let c = FMatrix::new_with_value(N, N, 3.0);
 
-        assert_eq!(new_mat, FMatrix::new_with_value(5, 5, 2.0));
+        let a = FMatrix::new_with_value(N, N, 1.0);
+        let b = FMatrix::new_with_value(N, N, 2.0);
+        assert_eq!(&a + &b, c);
+        assert_eq!(a + &b, c);
+
+        let a = FMatrix::new_with_value(N, N, 1.0);
+        assert_eq!(&a + b, c);
+
+        let a = FMatrix::new_with_value(N, N, 1.0);
+        let b = FMatrix::new_with_value(N, N, 2.0);
+        assert_eq!(a + b, c);
     }
 
     #[test]
-    fn mul_assign() {
-        let mut mat = FMatrix::new_with_value(5, 5, 1.0);
-        mat *= 2.0;
+    fn add_assign() {
+        let c = FMatrix::new_with_value(N, N, 3.0);
 
-        assert_eq!(mat, FMatrix::new_with_value(5, 5, 2.0));
+        let a = FMatrix::new_with_value(N, N, 1.0);
+        let mut b = FMatrix::new_with_value(N, N, 2.0);
+        b += &a;
+        assert_eq!(b, c);
+
+        let mut b = FMatrix::new_with_value(N, N, 2.0);
+        b += a;
+        assert_eq!(b, c);
+    }
+
+    #[test]
+    fn mul_scalar() {
+        let mat = FMatrix::new_with_value(N, N, 1.0);
+        assert_eq!(&mat * &2.0, FMatrix::new_with_value(N, N, 2.0));
+
+        assert_eq!(&mat * 2.0, FMatrix::new_with_value(N, N, 2.0));
+
+        let mat = FMatrix::new_with_value(N, N, 1.0);
+        assert_eq!(mat * &2.0, FMatrix::new_with_value(N, N, 2.0));
+
+        let mat = FMatrix::new_with_value(N, N, 1.0);
+        assert_eq!(mat * 2.0, FMatrix::new_with_value(N, N, 2.0));
+    }
+
+    #[test]
+    fn mul_assign_scalar() {
+        let mut mat = FMatrix::new_with_value(N, N, 1.0);
+
+        mat *= 2.0;
+        assert_eq!(mat, FMatrix::new_with_value(N, N, 2.0));
+
+        mat *= &2.0;
+        assert_eq!(mat, FMatrix::new_with_value(N, N, 4.0));
+    }
+
+    #[test]
+    fn sub() {
+        let c = FMatrix::new_with_value(N, N, 1.0);
+
+        let a = FMatrix::new_with_value(N, N, 3.0);
+        let b = FMatrix::new_with_value(N, N, 2.0);
+        assert_eq!(&a - &b, c);
+        assert_eq!(a - &b, c);
+
+        let a = FMatrix::new_with_value(N, N, 3.0);
+        assert_eq!(&a - b, c);
+
+        let a = FMatrix::new_with_value(N, N, 3.0);
+        let b = FMatrix::new_with_value(N, N, 2.0);
+        assert_eq!(a - b, c);
+    }
+
+    #[test]
+    fn sub_assign() {
+        let c = FMatrix::new_with_value(N, N, 1.0);
+
+        let a = FMatrix::new_with_value(N, N, 2.0);
+        let mut b = FMatrix::new_with_value(N, N, 3.0);
+        b -= &a;
+        assert_eq!(b, c);
+
+        let mut b = FMatrix::new_with_value(N, N, 3.0);
+        b -= a;
+        assert_eq!(b, c);
     }
 }
