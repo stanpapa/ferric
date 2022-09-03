7fbac55ddddee0cf6a7ff2f4f17c31821c36c515
Stan Papadopoulos <stan.papadopoulos@gmail.com>
2022-09-03 09:50:41 +0200

    Implemented diagonalization for FMatrixSym

--- lib/src/math/traits.rs
+++ lib/src/math/traits.rs
@@ -1,7 +1,9 @@
-use crate::math::matrix::Matrix;
-use crate::math::matrix_symmetric::MatrixSym;
+use crate::math::matrix::{FMatrix, Matrix};
+use crate::math::matrix_symmetric::{FMatrixSym, MatrixSym};
 use crate::math::scalar::Scalar;
-use crate::math::vector::Vector;
+use crate::math::vector::{FVector, Vector};
+
+use lapack::*;
 
 pub trait Dot<T: Scalar> {
     type Output;
@@ -76,3 +78,87 @@ impl<T: Scalar> From<&Matrix<T>> for MatrixSym<T> {
 //         self.len() == 0
 //     }
 // }
+
+pub trait Diagonalize {
+    type Output;
+
+    fn diagonalize(&self) -> Self::Output;
+}
+
+// // todo: merge FMatrix FMatrixSym impl (macro?)
+// impl Diagonalize for FMatrix {
+//     type Output = (FVector, FMatrix);
+//
+//     fn diagonalize(&self) -> Self::Output {
+//         if self.cols() != self.rows() {
+//             panic!("[diagonalize] trying to diagonalize a non-square matrix.");
+//         }
+//
+//         let n = self.cols();
+//         let mut info = 0;
+//         let mut eigenvalues = FVector::zero(n);
+//         let mut eigenvectors = self.clone();
+//
+//         // what is the purpose of this? Is this some kind of buffer for
+//         // the diagonalization?
+//         let lwork = 3 * n - 1;
+//         let mut work = FVector::zero(lwork);
+//
+//         // todo: careful this assumes a symmetric matrix
+//         unsafe {
+//             dsyev(
+//                 b'V',
+//                 b'U',
+//                 n as i32,
+//                 eigenvectors.as_mut_slice(),
+//                 n as i32,
+//                 eigenvalues.as_mut_slice(),
+//                 work.as_mut_slice(),
+//                 lwork as i32,
+//                 &mut info,
+//             );
+//         }
+//
+//         if info != 0 {
+//             panic!("Diagonalization failed with error code: {}", info);
+//         }
+//
+//         (eigenvalues, eigenvectors)
+//     }
+// }
+
+impl Diagonalize for FMatrixSym {
+    type Output = (FVector, FMatrix);
+
+    fn diagonalize(&self) -> Self::Output {
+        let n = self.cols();
+        let mut info = 0;
+        let mut eigenvalues = FVector::zero(n);
+        let mut eigenvectors = FMatrix::from(self);
+
+        // what is the purpose of this? Is this some kind of buffer for
+        // the diagonalization?
+        let lwork = 3 * n - 1;
+        let mut work = FVector::zero(lwork);
+
+        unsafe {
+            dsyev(
+                b'V',
+                b'U',
+                n as i32,
+                eigenvectors.as_mut_slice(),
+                n as i32,
+                eigenvalues.as_mut_slice(),
+                work.as_mut_slice(),
+                lwork as i32,
+                &mut info,
+            );
+        }
+
+        if info != 0 {
+            panic!("Diagonalization failed with error code: {}", info);
+        }
+
+        (eigenvalues, eigenvectors)
+    }
+}
